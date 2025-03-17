9e23a1786a07e2bea0473a2a6170fca437108464
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-16 23:34:08 -0300

    excebolinha

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string frase = "Eu troco as letras, não falo bem, mas meus amigos não troco por ninguém";
+Console.WriteLine(frase);
+Console.Write($"Nova frase: {frase.Replace('r', 'l').Replace('R', 'L')}");
