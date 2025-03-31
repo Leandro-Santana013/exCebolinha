d2ec3e416dbe8f9004cb0274c15367719eb6b5b2
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-30 23:31:13 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 String frase = ""
 Console.write("Digite uma frase: ")
-frase = Console.Readline();
+frase = Console.Readline()!;
 
 Console.Write($"Nova frase: {frase.Replace('r', 'l').Replace('R', 'L')}");
